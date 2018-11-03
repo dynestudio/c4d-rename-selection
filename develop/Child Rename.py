c15e6b800481e82ff2edc9c5c33723d3a4a08039
Nandita <carlos@dynetv.com>
2018-11-03 14:10:29 -0300

    v03

--- develop/Child Rename.py
+++ develop/Child Rename.py
@@ -8,11 +8,11 @@ class OptionsDialog(gui.GeDialog):
 
     def CreateLayout(self):
 
-        self.SetTitle('Crypto User AOV')
+        self.SetTitle('Rename Selection')
 
-        self.AddStaticText(self.IDC_LABELNAME, c4d.BFH_LEFT, name='Set User Data Name:') 
+        self.AddStaticText(self.IDC_LABELNAME, c4d.BFH_LEFT, name='Set the new selection name:') 
         self.AddEditText(self.IDC_EDITNAME, c4d.BFH_SCALEFIT)
-        self.SetString(self.IDC_EDITNAME, 'Write Crypto User Data AOV')
+        self.SetString(self.IDC_EDITNAME, 'Write you selection new name')
 
         # Ok/Cancel buttons
         self.AddDlgGroup(c4d.DLG_OK|c4d.DLG_CANCEL)
@@ -28,17 +28,59 @@ class OptionsDialog(gui.GeDialog):
 
         elif id == c4d.IDC_CANCEL:
             self.Close()
-            gui.MessageDialog('Please select a user Cryptomatte input user data.')
+            gui.MessageDialog('Please write a name, if you want you can leave empty to get only numers too.')
 
         return True
 
-def main():
+def no_sel_dlg():
+    gui.MessageDialog('Please select one or more objects / materials.')
+    return 
 
-    # get Active Objects
+def get_active_objs():
+    # get active objects
     activeObjects = doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_CHILDREN)
     if not activeObjects:
-        gui.MessageDialog('Please select one or more objects.')
-        return
+        no_sel_dlg()
+        return None
+
+    return activeObjects
+
+def get_active_mats():
+    # get active materials
+    activeMaterials = doc.GetActiveMaterials()
+    if not activeMaterials:
+        no_sel_dlg()
+        return None
+
+    return activeMaterials
+
+def main():
+    # get active selection 
+    sel_objs = get_active_objs()
+    sel_type = 0
+
+    if sel_objs == None:
+        sel_objs = get_active_mats()
+        sel_type = 1
+
+        if sel_objs == None:
+            no_sel_dlg()
+            return
+
+    # key input event
+    bc = c4d.BaseContainer()
+    if c4d.gui.GetInputState(c4d.BFM_INPUT_KEYBOARD,c4d.BFM_INPUT_CHANNEL,bc):
+        if  bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QSHIFT:
+            print "shift input"
+
+        elif  bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QALT: 
+            print "alt input"
+
+        elif  bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QCTRL: 
+            print "ctrl input"
+
+        else:
+            None
 
     # Open the options dialog to let users choose their options.
     dlg = OptionsDialog()
@@ -46,13 +88,20 @@ def main():
     if not dlg.ok:
         return
 
-    dialog = dlg.findGName
+    # new selection nanme
+    sel_name_new = dlg.findGName
 
-    i = 0
 
-    for obj in activeObjects:
+    # wip to do:
+    # que reconozca el final del string para a;adir un _ o - o espacio en caso que no haya nada en el string del dialog
+    # sirva tambien para materiales
+    # BaseDocument.GetActiveMaterials
+
+    i = 0 # number iterator
+
+    for obj in sel_objs:
         i += 1
-        obj[c4d.ID_BASELIST_NAME] = dialog + "0" + str(i)
+        obj[c4d.ID_BASELIST_NAME] = sel_name_new + "0" + str(i)
 
     c4d.EventAdd()
     
